3ef66dc6e5bee423c3c7f129a3d2a7f0ce5cb125
gilles <gilles@dassac.com>
2025-08-07 09:35:22 +0200

    update

--- build.sh
+++ build.sh
@@ -19,14 +19,38 @@ make install
 echo "Building Redis..."
 cd "$ROOT_DIR/redis"
 make -j4 PREFIX="$DIST_DIR/redis" install
-make test
+#make test
 
-# Build Redis
+# Build Garnet
 echo "Building Garnet..."
 cd "$ROOT_DIR/garnet"
 dotnet restore
-dotnet build -c Release -o "$DIST_DIR/garnet"
+dotnet publish main/GarnetServer/GarnetServer.csproj -c Release -o "$DIST_DIR/garnet"  --framework "net9.0" -p:PublishSingleFile=true #--self-contained true --runtime osx-arm64 
+ 
 
+# Build NATS Server
+echo "Building NATS Server..."
+cd "$ROOT_DIR/nats-server"
+go build -o "$DIST_DIR/nats-server/nats-server"
+
+
+# Build SeaweedFS
+echo "Building SeaweedFS..."
+cd "$ROOT_DIR/seaweedfs"
+cd "weed"
+go build -o "$DIST_DIR/seaweedfs/seaweedfs"
+
+
+
+# Build traefik
+echo "Building traefik..."
+cd "$ROOT_DIR/traefik"
+make -j4 PREFIX="$DIST_DIR/traefik"
+
+# Build influxdb
+echo "Building traefik..."
+cd "$ROOT_DIR/influxdb"
+cargo build --all-targets --target-dir ../dist/influxdb --profile quick-release
 
 
 
